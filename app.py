a77d08273360ffdb3baf2a00737dcf769d6ddcaa
pth00829 <pth030829@naver.com>
2026-09-09 14:08:34 +0900

    data.txt를 이용해서 로그인 아이디, 비번 비교, 회원가입시 정보 저장, 탈퇴 기능 추가

--- app.py
+++ app.py
@@ -10,14 +10,61 @@ def move():
 
 @app.route('/sign_up',methods=["POST","GET"])
 def user():
+    if request.method=="POST":
+        id=request.form.get('new_id')
+        pw=request.form.get('new_pw')
+        with open("data.txt", "r") as f:
+            saved_data=f.readlines()
+            for line in saved_data:
+                parts=line.strip().split(":")
+                saved_id,saved_pw=parts
+                if saved_id==id:
+                    return render_template('sign_up.html')
+            new_data=f"{id}:{pw}\n"
+
+        with open("data.txt", "a") as f:
+            f.write(new_data)
+        return render_template("index.html")
     return render_template('sign_up.html')
 
 @app.route('/login',methods=["POST","GET"])
 def compare_data():
+    if request.method=="POST":
+        id=request.form.get('id')
+        pw=request.form.get('pw')
+
+        with open("data.txt","r") as f:
+            saved_data=f.readlines()
+            for line in saved_data:
+                parts=line.strip().split(":")
+                saved_id,saved_pw=parts
+                if saved_id==id and saved_pw==pw:
+                    return render_template('success_login.html')
+
     return render_template('login.html')
 
 @app.route('/secession',methods=["POST","GET"])
 def delete_data():
+    if request.method=="POST":
+        id=request.form.get("delete_id")
+        pw=request.form.get("delete_pw")
+        new_data=[]
+
+        with open("data.txt", "r") as f:
+            data=f.readlines()
+            for line in data:
+                parts=line.strip().split(":")
+                saved_id,saved_pw=parts
+
+                if id==saved_id and pw==saved_pw:
+                    continue
+                else:
+                    new_data.append(line)
+
+        with open("data.txt", "w") as f:
+            f.writelines(new_data)
+        return render_template('index.html')
+
     return render_template('secession.html')
 
 if __name__=="__main__":
